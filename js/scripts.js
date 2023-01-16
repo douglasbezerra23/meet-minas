b7d1717dd0e8771ec184b9d3aee2837bb88dfd6c
Douglas <douglas13081995@gmail.com>
2023-01-16 20:54:25 +0100

    atualizando o js

--- js/scripts.js
+++ js/scripts.js
@@ -57,7 +57,18 @@ let _sliderCounter = 0, _touchStart, _touchEnd;
 
 _elements.date.innerHTML = new Date().getFullYear() + ".";
 _elements.scrollLinks.forEach(link => {
+	link.addEventListener("click", e => {
+		const id= link.getAttribute("href");
+		const element = document.querySelector(id);
+		const position = element.offsetTop - 62;
 
+		window.scrollTo({
+			top: position,
+			behavior: "smooth"
+		});
+
+		e.preventDefault();
+	});
 });
 
 _elements.toggle.addEventListener("click", () => {
